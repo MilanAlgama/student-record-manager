1e3a1b432778200601cf6ad9ec7bd43c7d56447a
MilanAlgama <milanharsha28@gmail.com>
2026-05-04 13:57:38 +0530

    Implemented deleteStudents feature

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ public class Main {
         addStudent("Alice");
         addStudent("Bob");
         viewStudents();
+        deleteStudent("Alice");
     }
 
     //Add Students
@@ -22,5 +23,11 @@ public class Main {
             for (String s : students) {
                 System.out.println(s);
             }
+    }
+
+    //Delete Students
+    static void deleteStudent(String name) {
+        students.remove(name);
+        System.out.println(name + " removed.");
     }
 }
